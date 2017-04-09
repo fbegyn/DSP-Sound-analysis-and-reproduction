6959f47ec13c9eeca1d561701ad8f68ec7512e1c
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-04-09 15:44:40 +0200

    WIP on iterating over sound

--- python/testFunctions.py
+++ python/testFunctions.py
@@ -1,4 +1,4 @@
-from functions import *
+#from functions import *
 
 ### Test the plotFFT functions
 #fs=800
@@ -6,8 +6,35 @@ from functions import *
 #plot(abs(fft(signal)))
 #plotFFT(signal,fs)
 
-env = ASD_envelope(3000,.05,.8,.4,2.4,5,1.5)
-tEnv = np.linspace( 0, 3000, len(env) )
-plt.plot( tEnv, env )
-plt.savefig( "testOutputs/EnvASD.png" )
-plt.close()
+#env = ASD_envelope(3000,.05,.8,.4,2.4,5,1.5)
+#tEnv = np.linspace( 0, 3000, len(env) )
+#plt.plot( tEnv, env )
+#plt.savefig( "testOutputs/EnvASD.png" )
+#plt.close()
+
+from functions import *
+from sign import *
+from fft import *
+
+# Experiment imports, if using permanent, put them above
+import numpy as np
+from scipy.signal import argrelmax,argrelextrema
+
+###############################################################################
+#                           Input of sample sound                             #
+###############################################################################
+##### Read the input file
+inp = Signal()
+inp.from_file('sampleSounds/galop02.wav')
+inp.write('testOutputs/original.wav')
+inp.info()
+inp.plotfft()
+
+##### Settings for iterating
+sample_length = 1024
+sample_overlap = 512
+
+##### Make signal a multiple of sample_length
+extra = np.zeros(int(inp.get_len() % sample_length))
+inp.signal = np.concatenate( [inp.signal,extra] )
+# WIP
