90c8b0cccd71bd3e2f8baabbb90a5d2c4a3c7adf
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-04-09 15:41:00 +0200

    Changed clean_noise methode
    Added noise level variable

--- python/fft.py
+++ python/fft.py
@@ -5,6 +5,9 @@ from scipy.signal import argrelmax
 
 class FFT:
 
+    ###########################################################################
+    #                          Signal input methodes                          #
+    ###########################################################################
     def __init__(self, Signal):
         self.__signal = Signal.signal
         self.__samplerate = Signal.get_fs()
@@ -14,26 +17,40 @@ class FFT:
         print('Signal: ', self.__signal)
         print('Samplerate: ',self.__samplerate)
 
+    ###########################################################################
+    #                         Signal output methodes                          #
+    ###########################################################################
     def ifft(self):
         return ifft(self.fft)
 
+    ###########################################################################
+    #                            Information output                           #
+    ###########################################################################
+    def plot(self):
+        # Shift the right part of the fft to the left, so it will plot correctly
+    	# and rescale the X-axis
+        dataY=np.abs(fftshift(self.fft))
+        dataX=fftshift(fftfreq(len(self.__signal),1./self.__samplerate))
+        plt.figure()
+        plot = plt.plot(dataX,dataY)
+        plt.grid()
+        plt.show()
+
+    ###########################################################################
+    #                         FFT processing methodes                         #
+    ###########################################################################
     def normalize(self):
+        # Rescale the fft into range 0..1
         factor = np.ndarray.max(self.fft)
         self.fft /= factor
         return factor
 
-    def clean_noise(self):
-        self.fft[self.fft<.3]=0
-
-    def plot(self):
-        dataY=np.abs(fftshift(self.fft))
-        dataX=fftshift(fftfreq(len(self.__signal),1./self.__samplerate))
-        plt.figure()
-        plot=plt.plot(dataX,dataY)
-        plt.grid()
-        plt.show()
+    def clean_noise(self,level=.3):
+        # Delete all noise values lower than 'level'
+        self.fft[self.fft<level]=0
 
     def find_freq(self):
+        # Find the max values of the fft
         return argrelmax(self.fft[:len(self.fft)/2],order=2)[0]/2
 
     def get_amplitudes(self,freq):
