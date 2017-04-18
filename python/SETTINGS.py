f7d9cd4c8129fe22fa5568a91529e389e0ded871
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-04-18 21:25:28 +0200

    I've put all settings together in one place

---
+++ python/SETTINGS.py
@@ -0,0 +1,39 @@
+
+""" SETTINGS FOR SYNTHESISING SIGNAL """
+
+###############################################################################
+#                            Input of sample sound                            #
+###############################################################################
+# File location
+INPUT_DIRECTORY='sampleSounds/'
+INPUT_FILENAME='galop02.wav'
+
+# Pick a sample out of the input sound
+# Change CUT_INPUT to False to disable
+CUT_INPUT = False
+CUT_INPUT_BEGIN = 0.58
+CUT_INPUT_END = 1.58
+
+###############################################################################
+#                                   Sampling                                  #
+###############################################################################
+SAMPLE_LENGTH = 352
+SAMPLE_OVERLAP = 0
+
+###############################################################################
+#                                  Synthesise                                 #
+###############################################################################
+# Change the sample rate
+# In the end we'll need 48000 sps
+NEW_FS = 44100 # We can't change this yet! (not fully implemented)
+
+# Maximum number of frequencies used to recreate sound
+# set to zero to disable limit
+MAX_FREQUENCIES = 0
+
+###############################################################################
+#                        Put samples together to output                       #
+###############################################################################
+# File location
+OUTPUT_DIRECTORY='testOutputs/'
+OUTPUT_FILENAME='synthesised.wav'
